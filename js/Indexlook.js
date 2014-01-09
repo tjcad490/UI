5d7024becaec2a37f01dcb87072552ec7ab6b2b1
xiaobai <492412921@qq.com>
2014-01-09 09:57:19 +0800

    create

---
+++ js/Indexlook.js
@@ -0,0 +1,9 @@
+﻿//$(function () {
+//    InitIndex();
+//})
+
+//function InitIndex() {
+//    var indexlist = "";
+//    indexlist += '<iframe name="myindex" scrolling="auto" frameborder="0"  src="Myindex.html" style="width:90%;height:100%;float:left"></iframe>';
+//    $(".home").append(indexlist);
+//}
---
+++ js/fenikso.js
@@ -0,0 +1,3 @@
+jQuery(function($){
+  $('#main [rel=popover]').popover()
+});
